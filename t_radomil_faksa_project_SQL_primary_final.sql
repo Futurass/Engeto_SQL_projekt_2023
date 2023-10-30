fd34b01ad6f82c9dd8770589c433406f5d96f1e3
Radomil Faksa <radomil.faksa@gmail.com>
2023-10-31 00:21:14 +0100

    Fix script & add more years
    Line 26 = vložen skript, který umožní provést následný INSERT
    
    Line 16, 38 = rozšířen interval o další roky

--- t_radomil_faksa_project_SQL_primary_final.sql
+++ t_radomil_faksa_project_SQL_primary_final.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS t_radomil_faksa_project_SQL_primary_final AS
 	WHERE 1=1
 		AND cp.value IS NOT NULL
 		AND cp.industry_branch_code IS NOT NULL
-		AND cp.payroll_year IN (2016, 2017, 2018)
+		AND cp.payroll_year IN (2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018)
 		AND cp.value_type_code = 5958
 		AND cp.calculation_code = 200
 	GROUP BY cp.industry_branch_code, cp.payroll_year
@@ -23,6 +23,7 @@ ALTER TABLE	t_radomil_faksa_project_SQL_primary_final ADD COLUMN commodity_name 
 ALTER TABLE	t_radomil_faksa_project_SQL_primary_final ADD COLUMN average_price_CZK DECIMAL(14, 4);
 ALTER TABLE	t_radomil_faksa_project_SQL_primary_final ADD COLUMN unit_value DOUBLE;
 ALTER TABLE	t_radomil_faksa_project_SQL_primary_final ADD COLUMN unit VARCHAR(2);
+ALTER TABLE	t_radomil_faksa_project_SQL_primary_final ALTER COLUMN industry_branch_name SET DEFAULT NULL;
 
 INSERT INTO t_radomil_faksa_project_SQL_primary_final (year, commodity_name, average_price_CZK, unit_value, unit)
 	SELECT
@@ -34,7 +35,7 @@ INSERT INTO t_radomil_faksa_project_SQL_primary_final (year, commodity_name, ave
 	FROM czechia_price cp2
 	LEFT JOIN czechia_price_category cpc ON cp2.category_code = cpc.code
 	WHERE 1=1
-		AND YEAR(cp2.date_from) IN (2016, 2017, 2018)
+		AND YEAR(cp2.date_from) IN (2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018)
 		AND YEAR(cp2.date_from) = YEAR(cp2.date_to)
 	GROUP BY cpc.name, YEAR(cp2.date_from) 
 ;
