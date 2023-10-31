f29126f3e15fb1125d6d83c8cfd04ec216f42f8f
Radomil Faksa <radomil.faksa@gmail.com>
2023-10-31 01:20:22 +0100

    Add more years
    Přidáno více roků pro pozorování

--- t_radomil_faksa_project_SQL_secondary_final.sql
+++ t_radomil_faksa_project_SQL_secondary_final.sql
@@ -33,5 +33,5 @@ CREATE TABLE IF NOT EXISTS t_radomil_faksa_project_SQL_secondary_final AS
 	FROM economies e
 	LEFT JOIN countries c ON c.country = e.country
 	WHERE 1=1
-		AND e.year IN (2016, 2017, 2018)
+		AND e.year IN (2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018)
 ;
